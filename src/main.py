51d6e20252d007fdc05cc9308e8b001188869661
safarzadehsbeengood <safarzadehryan@gmail.com>
2025-03-25 00:10:44 -0700

    ready for gh pages

--- src/main.py
+++ src/main.py
@@ -3,18 +3,26 @@ import shutil
 from markdown import *
 from gencontent import *
 from utils import *
+import sys
 
 static_path = "./static"
-public_path = "./public"
+public_path = "./docs"
 template_file = "./template.html"
 content_path = "./content"
 
 def main():
+    basepath = '/'
+    if len(sys.argv) >= 2:
+        basepath = sys.argv[1]
+
     print("deleting public directory...")
     if os.path.exists(public_path):
         shutil.rmtree(public_path)
+
     print('copying static files to public directory...')
     copy_files(static_path, public_path)
-    generate_pages_recursive(content_path, template_file, public_path)
+
+    print("generating pages...")
+    generate_pages_recursive(content_path, template_file, public_path, basepath)
 
 main()
